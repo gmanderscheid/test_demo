43e75bf0126adf71f993b11d534fb797a7ff3f8b
Guillaume Manderscheid <gmanderscheid@hotmail.fr>
2026-03-22 17:20:11 +0100

    adding tests

---
+++ test_app.py
@@ -0,0 +1,10 @@
+from app import square
+
+def test_square_positive():
+    assert square(3) == 9
+
+def test_square_zero():
+    assert square(0) == 0
+
+def test_square_negative():
+    assert square(-4) == 16
